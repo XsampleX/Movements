bf539a616a83d0310d75a7cf61f77d5c993ede5d
Sandro Machaidze <sandromachiadze33@gmail.com>
2017-04-20 20:55:51 +0400

    Movement Lesson

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 distanceFromTarget;
+	void Start ()
+    {
+        distanceFromTarget = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = target.position + distanceFromTarget;
+	}
+}
---
+++ Assets/Scripts/playermovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+    //public Transform player;
+    public Rigidbody playerBody;
+    Vector3 deltaPos;
+    public float speedMultiplier;
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        deltaPos.z = Input.GetAxis("Vertical");
+        deltaPos.x = Input.GetAxis("Horizontal");
+        //Debug.Log(Input.GetAxis("Vertical"));
+        // Debug.Log(Input.GetAxis("Horizon"));
+        //player.position = player.position + deltaPos;
+        //transform.position += deltaPos aris igive rac zeda; 
+    }
+
+    private void FixedUpdate()
+    {
+        //playerBody.position = playerBody.position + deltaPos;
+        playerBody.AddForce(deltaPos * speedMultiplier);
+    }
+}
